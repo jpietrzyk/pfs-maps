a3251903af9b3ed78a94340f4ab73c06a3e9cde4
Jacek Pietrzyk <jacpie3k@gmail.com>
2025-11-29 18:44:47 +0100

    Convert jsx to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import HereMap from "@/components/here-map";
-import PublicTransitToggle from "@/components/public-transit-toggle";
+import HereMap from "@/components/here-map.tsx";
+// import PublicTransitToggle from "@/components/public-transit-toggle.jsx"; // TODO: Convert to TypeScript
 
 function App() {
   return (
     <>
       <HereMap />
-      <PublicTransitToggle />
+      {/* <PublicTransitToggle /> */} {/* TODO: Convert to TypeScript */}
     </>
   );
 }
--- src/components/here-map-provider.jsx
+++ src/components/here-map-provider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { HereMapProvider } from '@/components/here-map-context';
+import App from '../App';
+import { HereMapProvider } from './here-map-context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
