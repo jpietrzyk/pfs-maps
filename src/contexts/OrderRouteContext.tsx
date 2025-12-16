c3c6bfdd4d881b615d75a8e7a27102f3bc6c677c
Jacek Pietrzyk <jacpie3k@gmail.com>
2025-12-16 21:45:37 +0100

    Fix build

--- src/contexts/OrderRouteContext.tsx
+++ src/contexts/OrderRouteContext.tsx
@@ -1,6 +1,5 @@
-// Reset: empty context
-export {};
-}
+import { createContext } from "react";
+import type { OrderRouteContextType } from "@/types/order-route";
 
 export const OrderRouteContext = createContext<
   OrderRouteContextType | undefined
