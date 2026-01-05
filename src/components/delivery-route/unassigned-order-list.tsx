ff25581d9ebd0f920f154eaeb5f0b48555e11bb6
Jacek Pietrzyk <jacpie3k@gmail.com>
2026-01-05 19:36:05 +0100

    Filter component ready

--- src/components/delivery-route/unassigned-order-list.tsx
+++ src/components/delivery-route/unassigned-order-list.tsx
@@ -60,7 +60,7 @@ export const UnassignedOrderList: React.FC<UnassignedOrderListProps> = ({
               items={unassignedOrders}
               strategy={verticalListSortingStrategy}
             >
-              <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+              <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-8 gap-3">
                 {unassignedOrders.map((order) => (
                   <li
                     key={order.id}
